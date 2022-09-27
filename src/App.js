6fe9d3f7976714d6b41e9d248204e0ea89a1215f
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-09-27 13:15:50 -0500

    install react and boostrap. remove boilerplate

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function App() {
+  return <div className="container-lg d-flex justify-content-center align-items-center min-vh-100">Template - React-Bootstrap</div>;
+}
