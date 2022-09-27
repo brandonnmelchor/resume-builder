618ff7cb9b339af2f202a2277ff0e7a65eb1d938
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-09-27 13:19:10 -0500

    create static input for components

---
+++ src/components/demo.js
@@ -0,0 +1,115 @@
+const demo = {
+  personal: {
+    firstName: "Brandonn",
+    lastName: "Melchor",
+    phone: "+19567396168",
+    email: "brandonnmelchor@gmail.com",
+    linkedIn: "linkedin.com/in/brandonnmelchor",
+    gitHub: "github.com/brandonnmelchor",
+  },
+  education: [
+    {
+      schoolName: "Western Governors University",
+      degree: "Bachelor of Science",
+      major: "Computer Science",
+      startDate: "Jan 2023",
+      endDate: "May 2024",
+    },
+    {
+      schoolName: "University of Texas Rio Grande Valley",
+      degree: "Master of Science",
+      major: "Health Informatics",
+      startDate: "Jan 2019",
+      endDate: "Dec 2019",
+    },
+    {
+      schoolName: "University of Texas Rio Grande Valley",
+      degree: "Bachelor of Science",
+      major: "Nursing",
+      startDate: "Aug 2011",
+      endDate: "May 2016",
+    },
+  ],
+  skills: {
+    languages: "JavaScript, CSS/SASS, HTML",
+    technologies: "React.js, Node.js, Bootstrap, Webpack, Parcel, Git, Bash, Google Cloud Platform, Google Search",
+  },
+  experience: [
+    {
+      company: "Oracle",
+      title: "Clinical Solutions Architect",
+      startDate: "Apr 2022",
+      endDate: "Present",
+      description: [
+        "Coordinated current state reviews of clinical workflows in collaboration with CIO and key stakeholders",
+        "Implemented Cerner solutions to healthcare facilities, designing scalable processes to optimize workflows",
+        "Automated clinical processes by 90% using Cerner solutions such as PowerOrders and PowerPlans",
+        "Facilitated unit and integration testing, user acceptance validation, and post production documentation",
+      ],
+    },
+    {
+      company: "Tech Mahindra",
+      title: "Clinical Solutions Team Lead",
+      startDate: "May 2021",
+      endDate: "Apr 2022",
+      description: [
+        "Provided technical and operational guidance to a cross-functional team of clinicians and engineers",
+        "Implemented Cerner solutions to healthcare facilities, designing scalable processes to optimize workflows",
+        "Served as a point of contact to help define user stories, user acceptance criteria, and task estimates",
+        "Prioritized user stories within an Agile environment, using tools such as Jira and ServiceNow",
+      ],
+    },
+    {
+      company: "DHR Health",
+      title: "Clinical Solutions Specialist",
+      startDate: "Sep 2018",
+      endDate: "May 2021",
+      description: [
+        "Automated clinical processes for 90% of all inpatient and ambulatory clinic locations using Cerner native tools",
+        "Coordinated the design and implementation of Covid-19 protocols, optimizing admission and treatment times for 95% of all incoming Covid-19 patients",
+      ],
+    },
+    {
+      company: "Universal Health Services",
+      title: "RN Clinical Informaticist",
+      startDate: "May 2016",
+      endDate: "Sep 2018",
+      description: ["Provided operational support to Cerner solutions, defining user stories and routing feature requests"],
+    },
+  ],
+  projects: [
+    {
+      projectName: "Forecast",
+      link: "https://brandonnmelchor.github.io/forecast/",
+      description: [
+        "Developed a responsive weather app using data from OpenWeather API to provide users a live forecast",
+        "Leveraged geolocation and location search data from Google Places/Maps API to ensure forecast accuracy",
+        "Implemented functions to continuously update page elements based on location and weather data",
+      ],
+    },
+    {
+      projectName: "Read Me",
+      link: "https://brandonnmelchor.github.io/read-me/",
+      description: [
+        "Designed a responsive library app that enables users to track their reading list and update their progress",
+        "Leveraged Web Storage API to store user data and create a seamless experience between each visit",
+        "Utilized functions to dynamically create cards for each book, adding event listeners and attributes",
+      ],
+    },
+    {
+      projectName: "Pixel Sketch",
+      link: "https://brandonnmelchor.github.io/pixel-sketch/",
+      description: [
+        "Created an interactive sketchpad app where users can select color modes and erase the canvas itself",
+        "Utilized functions to dynamically create the canvas, adding event listeners to each “pixel” for functionality",
+      ],
+    },
+    {
+      projectName: "Book Landing Page",
+      link: "https://brandonnmelchor.github.io/landing-page-book/",
+      description: ["Leveraged Bootstrap to create an interactive product landing page using dynamic modal components"],
+    },
+  ],
+};
+
+export default demo;
