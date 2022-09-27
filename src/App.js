8278f14725b721815bd73ffee5b1f5d58cd4d25f
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-09-27 14:15:56 -0500

    initialize footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="d-flex flex-column justify-content-center min-vh-100">
         <Nav />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default class Footer extends React.Component {
+  render() {
+    const github = "https://github.com/brandonnmelchor";
+    const linkedin = "https://www.linkedin.com/in/brandonnmelchor/";
+
+    return (
+      <footer className="bg-dark text-white mt-auto">
+        <div className="container-lg py-3">
+          <div className="row justify-content-center align-items-center">
+            <div className="col text-end">
+              <span>&copy; 2022 Brandonn Melchor </span>
+              <a className="text-white text-decoration-none ps-1" href={github} target="_blank" rel="noreferrer">
+                <i className="bi bi-github"></i>
+              </a>
+              <a className="text-white text-decoration-none ps-2" href={linkedin} target="_blank" rel="noreferrer">
+                <i className="bi bi-linkedin"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
