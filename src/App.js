53c520181025324c43ba5fb90c65dab87b45b756
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-09-27 14:55:22 -0500

    mock sizing for form and resume components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ export default class App extends React.Component {
     return (
       <div className="d-flex flex-column justify-content-center min-vh-100">
         <Nav />
+        <div className="container-lg px-4 py-5">
+          <div className="row justify-content-center align-items-center gap-5">
+            <div className="card col-5" style={{ height: "10px", backgroundColor: "gray" }}></div>
+            <div className="card col" style={{ width: "770px", height: "1000px", backgroundColor: "gray" }}></div>
+          </div>
+        </div>
         <Footer />
       </div>
     );
