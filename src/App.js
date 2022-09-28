e6122f392161853ec3d0710f630557cb9a5363d1
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-09-27 20:57:36 -0500

    test style and sizing for resume card

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ export default class App extends React.Component {
       <div className="d-flex flex-column justify-content-center min-vh-100">
         <Nav />
         <div className="container-lg px-4 py-5">
-          <div className="row justify-content-center align-items-center gap-5">
+          <div className="row justify-content-center align-items-center gap-4">
             <div className="card col-5" style={{ height: "10px", backgroundColor: "gray" }}></div>
-            <div className="card col" style={{ width: "770px", height: "1000px", backgroundColor: "gray" }}></div>
+            <div className="card col-6 justify-content-center align-items-center text-white shadow " id="resume">
+              TEST
+            </div>
           </div>
         </div>
         <Footer />
