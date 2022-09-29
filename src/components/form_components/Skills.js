2af117371870331f08046ac4292fde87cb5b556d
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-09-29 00:27:40 -0500

    implement functionality for skills form component

--- src/components/form_components/Skills.js
+++ src/components/form_components/Skills.js
@@ -2,6 +2,35 @@ import React from "react";
 
 export default class Skills extends React.Component {
   render() {
-    return <div className="w-100">Skills</div>;
+    const { languages, technologies } = this.props.skills;
+    const update = this.props.update;
+
+    return (
+      <div className="w-100">
+        <form>
+          <div className="mb-4">
+            <InputGroup label="Languages" id="languages" value={languages} update={update} length="100" />
+          </div>
+          <div>
+            <InputGroup label="Technologies" id="technologies" value={technologies} update={update} length="100" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+class InputGroup extends React.Component {
+  render() {
+    const { label, id, value, update, length } = this.props;
+
+    return (
+      <div>
+        <label htmlFor={id} className="form-label fw-bold">
+          {label}
+        </label>
+        <textarea className="form-control" id={id} value={value} onChange={update} maxLength={length} rows="3" />
+      </div>
+    );
   }
 }
