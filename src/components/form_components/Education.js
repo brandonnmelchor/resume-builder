96f2dff108aaec2cb3ca4c914b00532c21e27521
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-09-29 21:28:47 -0500

    initialize functionality to edit education entries

--- src/components/form_components/Education.js
+++ src/components/form_components/Education.js
@@ -1,22 +1,32 @@
 import React from "react";
+import { TextInput, NumberInput, SelectInput, CheckboxInput } from "./FormInputs";
 
 export default class Education extends React.Component {
-  render() {
-    const education = this.props.education;
+  constructor(props) {
+    super(props);
 
-    return (
-      <div className="w-100 d-flex flex-column gap-3">
-        {education.map((entry) => (
-          <EducationEntry key={entry.id} educationEntry={entry} />
-        ))}
-      </div>
-    );
+    this.state = { editMode: true };
+    // this.prevSection = this.prevSection.bind(this);
+  }
+
+  render() {
+    const editMode = this.state.editMode;
+    const education = this.props.education;
+    const secondEntry = education[1].id;
+    let display;
+
+    if (editMode) {
+      const entry = education.filter((entry) => entry.id === secondEntry)[0];
+      display = <EntryForm key={entry.id} educationEntry={entry} />;
+    } else display = education.map((entry) => <EntryCard key={entry.id} educationEntry={entry} />);
+
+    return <div className="w-100 d-flex flex-column gap-3">{display}</div>;
   }
 }
 
-class EducationEntry extends React.Component {
+class EntryCard extends React.Component {
   render() {
-    const { id, schoolName, degree, major, startDate, endDate } = this.props.educationEntry;
+    const { id, schoolName, degree, major, startMonth, startYear, endMonth, endYear } = this.props.educationEntry;
 
     return (
       <div className="card d-flex flex-row justify-content-between p-3">
@@ -26,23 +36,25 @@ class EducationEntry extends React.Component {
             {degree} in {major}
           </div>
           <div className="mt-3">
-            {startDate} - {endDate}
+            {startMonth} {startYear} - {endMonth} {endYear}
           </div>
         </div>
         <div>
           <button
             type="button"
             className="btn gray px-1"
-            onClick={() => {
-              console.log("test");
+            entry={id}
+            onClick={(event) => {
+              console.log(event.target.attributes.entry.value);
             }}>
             <i className="bi bi-pencil-square"></i>
           </button>
           <button
             type="button"
             className="btn gray px-1"
+            entry={id}
             onClick={(event) => {
-              console.log(event);
+              console.log("test");
             }}>
             <i className="bi bi-trash"></i>
           </button>
@@ -52,17 +64,58 @@ class EducationEntry extends React.Component {
   }
 }
 
-// class InputGroup extends React.Component {
-//   render() {
-//     const { label, type, id, value, update, length } = this.props;
+class EntryForm extends React.Component {
+  render() {
+    const { id, schoolName, degree, major, startMonth, startYear, endMonth, endYear } = this.props.educationEntry;
+    const currentYear = new Date().getFullYear();
 
-//     return (
-//       <div>
-//         <label htmlFor={id} className="form-label fw-bold">
-//           {label}
-//         </label>
-//         <input type={type} className="form-control" id={id} value={value} onChange={update} maxLength={length} />
-//       </div>
-//     );
-//   }
-// }
+    return (
+      <div className="w-100">
+        <form>
+          <div className="mb-4">
+            <TextInput label="School Name" type="text" id="schoolName" value={schoolName} update={DOMRectReadOnly} length="25" />
+          </div>
+          <div className="row justify-content-center align-items-center mb-4">
+            <div className="col">
+              <TextInput label="Degree" type="text" id="degree" value={degree} update={DOMRectReadOnly} length="25" />
+            </div>
+            <div className="col">
+              <TextInput label="Major" type="text" id="major" value={major} update={DOMRectReadOnly} length="25" />
+            </div>
+          </div>
+          <div className="row justify-content-center align-items-center mb-4">
+            <div className="col">
+              <SelectInput label="Start Month" id="startMonth" value={startMonth} update={DOMRectReadOnly} />
+            </div>
+            <div className="col">
+              <NumberInput label="Start Year" id="startYear" value={startYear} update={DOMRectReadOnly} min="1900" max={currentYear} />
+            </div>
+          </div>
+          <div className="row justify-content-center align-items-center mb-4">
+            <div className="col">
+              <SelectInput label="End Month" id="endMonth" value={endMonth} update={DOMRectReadOnly} />
+            </div>
+            <div className="col">
+              <NumberInput label="End Year" id="endYear" value={endYear} update={DOMRectReadOnly} min="1900" max={currentYear} />
+            </div>
+          </div>
+          <div className="d-flex justify-content-between">
+            <div>
+              <CheckboxInput label="I currently study here" id="noEndDate" update={(event) => console.log(event.target.checked)} />
+            </div>
+            <div>
+              <button
+                type="button"
+                className="btn btn-outline-secondary gray-border"
+                onClick={(event) => {
+                  console.log(event.target);
+                }}>
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
