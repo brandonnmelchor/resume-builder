994ddad4f873d2029e48f71600cfedf7c09c13fd
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-10-07 00:15:57 -0500

    Revert "Revert "initialize savePDF and resumePDF components""
    This reverts commit d54b57517aba31b8d45fd660d2e9a41d554c20b6.

--- src/components/form_components/SavePDF.js
+++ src/components/form_components/SavePDF.js
@@ -1,14 +1,27 @@
 import React from "react";
+import ResumePDF from "../ResumePDF";
+import { pdf } from "@react-pdf/renderer";
+import { saveAs } from "file-saver";
 
 export default class SavePDF extends React.Component {
+  constructor(props) {
+    super(props);
+    this.savePDF = this.savePDF.bind(this);
+  }
+
+  async savePDF() {
+    const blob = await pdf(<ResumePDF />).toBlob();
+    saveAs(blob, "cv.pdf");
+  }
+
   render() {
     return (
       <div className="w-100">
         <div className="fs-4 fw-bold gray">Save PDF</div>
         <hr className="mb-4" />
-        <div className="user-select-none" style={{ cursor: "pointer" }}>
+        <div className="user-select-none" onClick={this.savePDF} style={{ cursor: "pointer" }}>
           <span className="d-flex align-items-center fw-bold gray">
-            <i className="bi bi-printer fs-3 me-2"></i> Save as PDF
+            <i className="bi bi-file-earmark fs-3 me-2"></i> Save as PDF
           </span>
         </div>
       </div>
