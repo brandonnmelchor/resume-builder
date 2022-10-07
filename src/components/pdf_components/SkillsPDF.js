964c198a189c8d6a365e1507cfe177ab923861b4
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-10-07 13:29:47 -0500

    update skills default when no text

--- src/components/pdf_components/SkillsPDF.js
+++ src/components/pdf_components/SkillsPDF.js
@@ -9,18 +9,24 @@ export default class SkillsPDF extends React.Component {
         <div className="pdf-header fw-bold pdf-gray">Skills</div>
         <hr />
         <div className="pdf-content">
-          <div>
-            <span className="fw-bold">Languages:&emsp;&emsp;</span>
-            <span>{languages}</span>
-          </div>
-          <div>
-            <span className="fw-bold">Frameworks:&emsp;&ensp;</span>
-            <span>{frameworks}</span>
-          </div>
-          <div>
-            <span className="fw-bold">Technologies:&emsp;</span>
-            <span>{technologies}</span>
-          </div>
+          {languages.length > 0 && (
+            <div>
+              <span className="fw-bold">Languages:&emsp;&emsp;</span>
+              <span>{languages}</span>
+            </div>
+          )}
+          {frameworks.length > 0 && (
+            <div>
+              <span className="fw-bold">Frameworks:&emsp;&ensp;</span>
+              <span>{frameworks}</span>
+            </div>
+          )}
+          {technologies.length > 0 && (
+            <div>
+              <span className="fw-bold">Technologies:&emsp;</span>
+              <span>{technologies}</span>
+            </div>
+          )}
         </div>
       </div>
     );
--- src/components/resume_components/SkillsResume.js
+++ src/components/resume_components/SkillsResume.js
@@ -9,18 +9,24 @@ export default class SkillsResume extends React.Component {
         <div className="resume-header fw-bold gray">Skills</div>
         <hr />
         <div className="resume-content">
-          <div>
-            <span className="fw-bold">Languages:&emsp;&emsp;</span>
-            <span>{languages}</span>
-          </div>
-          <div>
-            <span className="fw-bold">Frameworks:&emsp;&ensp;</span>
-            <span>{frameworks}</span>
-          </div>
-          <div>
-            <span className="fw-bold">Technologies:&emsp;</span>
-            <span>{technologies}</span>
-          </div>
+          {languages.length > 0 && (
+            <div>
+              <span className="fw-bold">Languages:&emsp;&emsp;</span>
+              <span>{languages}</span>
+            </div>
+          )}
+          {frameworks.length > 0 && (
+            <div>
+              <span className="fw-bold">Frameworks:&emsp;&ensp;</span>
+              <span>{frameworks}</span>
+            </div>
+          )}
+          {technologies.length > 0 && (
+            <div>
+              <span className="fw-bold">Technologies:&emsp;</span>
+              <span>{technologies}</span>
+            </div>
+          )}
         </div>
       </div>
     );
