2bea838840ed9748e6c84e303a9993bf9aabe47e
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-10-07 09:34:18 -0500

    set pdf title and update text

--- src/components/form_components/Print.js
+++ src/components/form_components/Print.js
@@ -3,18 +3,19 @@ import ReactToPrint, { PrintContextConsumer } from "react-to-print";
 
 export default class Print extends React.Component {
   render() {
+    const { firstName, lastName } = this.props.resume.personal;
     const printRef = this.props.printRef;
 
     return (
       <div className="w-100">
-        <div className="fs-4 fw-bold gray">Save PDF</div>
+        <div className="fs-4 fw-bold gray">Save Resume</div>
         <hr className="mb-4" />
-        <ReactToPrint content={() => printRef}>
+        <ReactToPrint content={() => printRef} documentTitle={`${firstName}${lastName}_Resume`}>
           <PrintContextConsumer>
             {({ handlePrint }) => (
               <div className="user-select-none" onClick={handlePrint} style={{ cursor: "pointer" }}>
                 <span className="d-flex align-items-center fw-bold gray">
-                  <i className="bi bi-printer fs-3 me-2"></i> Save as PDF
+                  <i className="bi bi-printer fs-3 me-2"></i> Print
                 </span>
               </div>
             )}
