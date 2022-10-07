ba56a96357f1072ef71bf4624e891e8b36e63cb3
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-10-07 00:00:48 -0500

    Revert "Revert "update print section to savePDF""
    This reverts commit 3dc81e1200f78079a5c9cb6c73b5871b2e3a4905.

---
+++ src/components/form_components/SavePDF.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ResumePDF from "../ResumePDF";
+import { pdf } from "@react-pdf/renderer";
+import { saveAs } from "file-saver";
+
+export default class SavePDF extends React.Component {
+  constructor(props) {
+    super(props);
+    this.savePDF = this.savePDF.bind(this);
+  }
+
+  async savePDF() {
+    const resume = this.props.resume;
+    const { firstName, lastName } = resume.personal;
+    const fileName = `${firstName}${lastName}_Resume.pdf`;
+
+    const blob = await pdf(<ResumePDF resume={resume} />).toBlob();
+    saveAs(blob, fileName);
+  }
+
+  render() {
+    return (
+      <div className="w-100">
+        <div className="fs-4 fw-bold gray">Save PDF</div>
+        <hr className="mb-4" />
+        <div className="user-select-none" onClick={this.savePDF} style={{ cursor: "pointer" }}>
+          <span className="d-flex align-items-center fw-bold gray">
+            <i className="bi bi-file-earmark fs-3 me-2"></i> Save as PDF
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
