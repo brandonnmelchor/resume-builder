3418d8e98ab1b08aadc48982c037fb110fb86030
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-10-07 11:19:38 -0500

    add pdf styles to personal section

--- src/components/pdf_components/PersonalPDF.js
+++ src/components/pdf_components/PersonalPDF.js
@@ -7,14 +7,14 @@ export default class PersonalResume extends React.Component {
 
     return (
       <div className="w-100 mb-2">
-        <div className="section-header fw-bold gray">
+        <div className="pdf-header fw-bold gray">
           {firstName} {lastName}
         </div>
         <hr />
-        <div className="section-content">
+        <div className="pdf-content">
           {phone} | {email}
         </div>
-        <div className="section-content">
+        <div className="pdf-content">
           {linkedin} | {github}
         </div>
       </div>
