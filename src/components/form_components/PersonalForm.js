82e44c368faf20c1af8657e6dd72144c2d426982
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-10-11 17:29:04 -0500

    add mui components

--- src/components/form_components/PersonalForm.js
+++ src/components/form_components/PersonalForm.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { TextInput } from "./FormInputs";
+
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import TextField from "@mui/material/TextField";
 
 export default class PersonalForm extends React.Component {
   constructor(props) {
@@ -16,32 +21,94 @@ export default class PersonalForm extends React.Component {
     const { firstName, lastName, phone, email, linkedin, github } = this.props.resume.personal;
 
     return (
-      <div className="w-100">
-        <div className="fs-4 fw-bold gray">Personal</div>
-        <hr className="mb-4" />
-        <form>
-          <div className="row justify-content-center align-items-center mb-4">
-            <div className="col">
-              <TextInput label="First Name" type="text" id="firstName" value={firstName} handleChange={this.handleChange} length="25" />
-            </div>
-            <div className="col">
-              <TextInput label="Last Name" type="text" id="lastName" value={lastName} handleChange={this.handleChange} length="25" />
-            </div>
-          </div>
-          <div className="mb-4">
-            <TextInput label="Phone" type="tel" id="phone" value={phone} handleChange={this.handleChange} length="25" />
-          </div>
-          <div className="mb-4">
-            <TextInput label="Email" type="email" id="email" value={email} handleChange={this.handleChange} length="50" />
-          </div>
-          <div className="mb-4">
-            <TextInput label="LinkedIn" type="url" id="linkedin" value={linkedin} handleChange={this.handleChange} length="50" />
-          </div>
-          <div>
-            <TextInput label="GitHub" type="url" id="github" value={github} handleChange={this.handleChange} length="50" />
-          </div>
-        </form>
-      </div>
+      <Box>
+        <Typography variant="h5" className="gray" sx={{ fontWeight: 500 }} component="h2">
+          Personal
+        </Typography>
+        <Divider className="form-divider" sx={{ mb: 3 }} />
+        <Box component="form">
+          <Grid container direction="row" justifyContent="center" alignItems="center" spacing={3} sx={{ mb: 3 }}>
+            <Grid item xs={6}>
+              <TextField
+                type="text"
+                variant="outlined"
+                size="small"
+                fullWidth
+                id="firstName"
+                label="First Name"
+                value={firstName}
+                onChange={this.handleChange}
+                inputProps={{ maxLength: 25 }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                type="text"
+                variant="outlined"
+                size="small"
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                value={lastName}
+                onChange={this.handleChange}
+                inputProps={{ maxLength: 25 }}
+              />
+            </Grid>
+          </Grid>
+          <Box sx={{ mb: 3 }}>
+            <TextField
+              type="tel"
+              variant="outlined"
+              size="small"
+              fullWidth
+              id="phone"
+              label="Phone"
+              value={phone}
+              onChange={this.handleChange}
+              inputProps={{ maxLength: 25 }}
+            />
+          </Box>
+          <Box sx={{ mb: 3 }}>
+            <TextField
+              type="email"
+              variant="outlined"
+              size="small"
+              fullWidth
+              id="email"
+              label="Email"
+              value={email}
+              onChange={this.handleChange}
+              inputProps={{ maxLength: 40 }}
+            />
+          </Box>
+          <Box sx={{ mb: 3 }}>
+            <TextField
+              type="url"
+              variant="outlined"
+              size="small"
+              fullWidth
+              id="linkedin"
+              label="LinkedIn"
+              value={linkedin}
+              onChange={this.handleChange}
+              inputProps={{ maxLength: 40 }}
+            />
+          </Box>
+          <Box>
+            <TextField
+              type="url"
+              variant="outlined"
+              size="small"
+              fullWidth
+              id="github"
+              label="GitHub"
+              value={github}
+              onChange={this.handleChange}
+              inputProps={{ maxLength: 40 }}
+            />
+          </Box>
+        </Box>
+      </Box>
     );
   }
 }
