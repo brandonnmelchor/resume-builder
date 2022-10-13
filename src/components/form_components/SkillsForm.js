f4555e89487fc15b58023234f5dd37ab42428bae
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-10-12 21:14:49 -0500

    add FormNav components

--- src/components/form_components/SkillsForm.js
+++ src/components/form_components/SkillsForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { SectionNavA } from "./FormNav";
 
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
@@ -9,6 +10,7 @@ export default class SkillsForm extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
@@ -16,8 +18,15 @@ export default class SkillsForm extends React.Component {
     handleInput(event, "skills");
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    const nextSection = this.props.sectionNav.nextSection;
+    nextSection();
+  }
+
   render() {
     const { languages, frameworks, technologies } = this.props.resume.skills;
+    const sectionNav = this.props.sectionNav;
 
     return (
       <Box>
@@ -25,7 +34,7 @@ export default class SkillsForm extends React.Component {
           Skills
         </Typography>
         <Divider className="form-divider" sx={{ mb: 5 }} />
-        <Box component="form">
+        <Box component="form" onSubmit={this.handleSubmit}>
           <Box sx={{ mb: 4 }}>
             <GrayTextField
               type="text"
@@ -68,6 +77,7 @@ export default class SkillsForm extends React.Component {
               inputProps={{ maxLength: 100 }}
             />
           </Box>
+          <SectionNavA sectionNav={sectionNav} />
         </Box>
       </Box>
     );
