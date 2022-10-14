486c66470df2ae356a1a7251e6f85fbe6bce87b0
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-10-14 16:15:03 -0500

    create custom hooks

---
+++ src/components/customHooks.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+export function useWindowSize() {
+  const [windowSize, setWindowSize] = useState(() => window.innerWidth);
+
+  const handleResize = () => {
+    setWindowSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return windowSize;
+}
