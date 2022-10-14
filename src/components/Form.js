97dff4e49ac2b57a9a4e857b2b979b61198f5fa8
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-10-14 15:54:04 -0500

    update to functional components and use state hook

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PersonalForm from "./form_components/PersonalForm";
 import EducationForm from "./form_components/EducationForm";
 import SkillsForm from "./form_components/SkillsForm";
@@ -7,50 +7,34 @@ import ProjectsForm from "./form_components/ProjectsForm";
 
 import Grid from "@mui/material/Grid";
 
-export default class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { section: 1 };
+export default function Form(props) {
+  const [section, setSection] = useState(() => 1);
+  const resume = props.resume;
+  const entryMode = props.entryMode;
+  const handleChange = props.handleChange;
 
-    this.prevSection = this.prevSection.bind(this);
-    this.nextSection = this.nextSection.bind(this);
-  }
+  const sectionNav = {
+    section,
 
-  prevSection() {
-    this.setState((state) => {
-      return { section: state.section - 1 };
-    });
-  }
+    prevSection() {
+      setSection((section) => section - 1);
+    },
 
-  nextSection() {
-    this.setState((state) => {
-      return { section: state.section + 1 };
-    });
-  }
+    nextSection() {
+      setSection((section) => section + 1);
+    },
+  };
 
-  render() {
-    const section = this.state.section;
-    const sectionNav = {
-      section: this.state.section,
-      prevSection: this.prevSection,
-      nextSection: this.nextSection,
-    };
+  let display;
+  if (section === 1) display = <PersonalForm resume={resume} handleChange={handleChange} sectionNav={sectionNav} />;
+  else if (section === 2) display = <EducationForm resume={resume} entryMode={entryMode} handleChange={handleChange} sectionNav={sectionNav} />;
+  else if (section === 3) display = <SkillsForm resume={resume} handleChange={handleChange} sectionNav={sectionNav} />;
+  else if (section === 4) display = <ExperienceForm resume={resume} entryMode={entryMode} handleChange={handleChange} sectionNav={sectionNav} />;
+  else if (section === 5) display = <ProjectsForm resume={resume} entryMode={entryMode} handleChange={handleChange} sectionNav={sectionNav} />;
 
-    const resume = this.props.resume;
-    const entryMode = this.props.entryMode;
-    const handleChange = this.props.handleChange;
-
-    let display;
-    if (section === 1) display = <PersonalForm resume={resume} handleChange={handleChange} sectionNav={sectionNav} />;
-    else if (section === 2) display = <EducationForm resume={resume} entryMode={entryMode} handleChange={handleChange} sectionNav={sectionNav} />;
-    else if (section === 3) display = <SkillsForm resume={resume} handleChange={handleChange} sectionNav={sectionNav} />;
-    else if (section === 4) display = <ExperienceForm resume={resume} entryMode={entryMode} handleChange={handleChange} sectionNav={sectionNav} />;
-    else if (section === 5) display = <ProjectsForm resume={resume} entryMode={entryMode} handleChange={handleChange} sectionNav={sectionNav} />;
-
-    return (
-      <Grid item sm={10} lg={5} id="form" mr={5} p={2}>
-        {display}
-      </Grid>
-    );
-  }
+  return (
+    <Grid item sm={10} lg={5} id="form" mr={5} p={2}>
+      {display}
+    </Grid>
+  );
 }
